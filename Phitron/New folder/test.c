c45bccf5d4876eb6077cae795f7c5ef4dc50ffe7
Nurul Arefin Nabil <arefinnabil256@gmail.com>
2025-04-05 16:37:19 +0600

    re test

--- Phitron/New folder/test.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main(){
-    printf("Hello World!\nThis is Nabil Arefin with his new journey!\nPlease pray for me..!");
-    printf("I am from Gazipur at Dhaka.\nThis is my basic inro.");
-    return 0;
-    }

23c647ba32935b01572bc45ff1eb5b501c154cbb
Nurul Arefin Nabil <arefinnabil256@gmail.com>
2025-04-05 16:27:27 +0600

    5th april class

--- Phitron/M_1-Introduction to C/intro.c
+++ Phitron/M_1-Introduction to C/intro.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 int main(){
     printf("Hello World!\nThis is Nabil Arefin with his new journey!\nPlease pray for me..!");
+    printf("I am from Gazipur at Dhaka.\nThis is my basic inro.");
     return 0;
     }

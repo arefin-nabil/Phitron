4047f36bb8f7c970d9cea494ad8997bb650faa2b
Nurul Arefin Nabil <arefinnabil256@gmail.com>
2025-04-09 22:26:11 +0600

    practice loop

--- Phitron/Introduction to Programming Language/M_3-Loop/for_loop.c
+++ Phitron/Introduction to Programming Language/M_3-Loop/for_loop.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main ()
 {
-    for (int i = 2 ; i<=10 ; i=i+2)
+    for (int i = 1 ; i<=10 ; i++)
     {
         printf("%d\n", i);
     }
---
+++ Phitron/Introduction to Programming Language/M_3-Loop/phitronOJ.c
@@ -0,0 +1,122 @@
+// Zero or Non Zero
+
+// #include<stdio.h>
+// int main ()
+// {
+//     int N;
+//     scanf("%d", &N);
+//     if (N==0)printf("Zero");
+//     else printf("Non Zero");
+//     return 0;
+// }
+
+
+
+
+// 5 Marks for good hand writing
+
+// #include<stdio.h>
+// int main()
+// {
+//     int N;
+//     scanf ("%d", &N);
+//     printf("%d", N+5);
+//     return 0;
+// }
+
+
+
+
+// Multiple or not.
+
+// #include <stdio.h>
+// int main()
+// {
+//     int a, b;
+//     scanf("%d %d", &a, &b);
+//     if (a % b == 0 || b % a == 0) printf("Yes");
+//     else printf("No");
+//     return 0;
+// }
+
+
+
+
+// Floating Point Number
+
+// #include <stdio.h>
+// int main()
+// {
+//     float x;
+//     scanf("%f", &x);
+//     printf("%.3f", x);
+//     return 0;
+// }
+
+
+
+
+// N Times
+
+// #include<stdio.h>
+// int main ()
+// {
+//     int N;
+//     scanf ("%d", &N);
+//     for (int i=1; i<=N; i++) printf("I Love Practice\n");
+//     return 0;
+// }
+
+
+
+// Variables
+
+// #include<stdio.h>
+// int main ()
+// {
+//     int A;
+//     long long int B;
+//     float C;
+//     char D;
+//     scanf("%d", &A);
+//     scanf("%lld", &B);
+//     scanf("%f\n", &C);
+//     scanf("%c", &D);
+//     printf("%d\n", A);
+//     printf("%lld\n", B);
+//     printf("%.2f\n", C);
+//     printf("%c\n", D);
+//     return 0;
+// }
+
+
+
+
+// Sum of Two Numbers
+
+// #include <stdio.h>
+// int main()
+// {
+//     int A, B;
+//     scanf("%d %d", &A, &B);
+//     printf("%d", A+B);
+//     return 0;
+// }
+
+
+
+
+// Divisible By 5 or Not
+
+// #include<stdio.h>
+// int main()
+// {
+//     int N;
+//     scanf("%d", &N);
+//     for (int i=1; i<=N; i++)
+//     {
+//         if (i%5==0)printf("%d Yes\n", i);
+//         else printf("%d No\n", i);
+//     }
+//     return 0;
+// }

70605db84f45e8c5674f31b47acd6830b813ed9f
Nurul Arefin Nabil <arefinnabil256@gmail.com>
2025-04-14 23:04:21 +0600

    practice module 6

---
+++ Phitron/Q_Digits.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int x;
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%d", &x);
+        // printf("%d\n", x);
+
+        
+
+    }
+    
+    return 0;
+}
